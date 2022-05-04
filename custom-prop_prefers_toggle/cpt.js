18b8278e8bc37916de1857bae798fe83707f35fe
emarekica <emarekica@gmail.com>
2022-05-04 21:16:25 +0200

    config(html, css, js): add form validation feature

--- custom-prop_prefers_toggle/cpt.js
+++ custom-prop_prefers_toggle/cpt.js
@@ -1,4 +1,5 @@
 // with sessionStorage
+// with form validation
 
 "use strict";
 
@@ -52,4 +53,50 @@ themeSwitch.addEventListener("change", () => {
 });
 
 // --- FORM VALIDATION
-// add customizable error message
+// add customizable error message with Constraint validation API
+
+const form = document.getElementsByTagName("form");
+const email = document.getElementById("email");
+const emailError = document.querySelector("#email + span.error");
+
+email.addEventListener("input", function (event) {
+  // Each time the user types, check if the form fields are valid
+
+  if (email.validity.valid) {
+    // If there is an error message visible, if the field is valid, remove the error message
+    emailError.textContent = ""; // Reset the content of the message
+    emailError.className = "error"; // Reset the visual state of the message
+  } else {
+    // If there is still an error, show the correct error
+    showError();
+  }
+});
+
+form.addEventListener("submit", function (event) {
+  // if the email field is valid, we let the form submit
+
+  if (!email.validity.valid) {
+    // If it isn't, we display an appropriate error message
+    showError();
+    // Then we prevent the form from being sent by canceling the event
+    event.preventDefault();
+  }
+});
+
+function showError() {
+  if (email.validity.valueMissing) {
+    // If the field is empty, display error msg
+    emailError.textContent = "Please, enter an e-mail address.";
+  }
+  // If the field input doesn't contain an email address, display error msg
+  else if (email.validity.typeMismatch) {
+    emailError.textContent = "Entered input needs to be an e-mail address.";
+  }
+  // If the data is too short,ndisplay error msg
+  else if (email.validity.tooShort) {
+    emailError.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
+  }
+
+  // Set the styling appropriately
+  emailError.className = "error active";
+}

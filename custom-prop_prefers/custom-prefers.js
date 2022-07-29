03c82ffd10bd197b1aee29781a4091118c5b39ba
Marija <77061620+emarekica@users.noreply.github.com>
2022-07-29 19:35:27 +0200

    Delete custom-prop_prefers directory

--- custom-prop_prefers/custom-prefers.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const themeSwitch = document.querySelector(".checkbox");
-
-themeSwitch.addEventListener("change", () => {
-  document.body.classList.toggle("dark-theme");
-});
-
-// arrange toggle button with custom properties + prefers-color-scheme
